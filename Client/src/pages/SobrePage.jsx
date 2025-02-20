1c517fd6630445a2bd6034bb9df2f8a6274ea00b
Hender <henderrojas30570321@gmail.com>
2025-02-19 20:07:22 -0400

    sobre nosotros completa v1

--- Client/src/pages/SobrePage.jsx
+++ Client/src/pages/SobrePage.jsx
@@ -15,12 +15,13 @@ function SobrePage() {
         </span>
         <article className="mx-auto">
           <p className="text-justify m-2 font-bold text-xl">
-            En ARS Autocamiones, la tradición de ENCAVA se une a la atención
-            personalizada. Como concesionario oficial, te ofrecemos la gama
-            completa de vehículos ENCAVA, desde autobuses hasta camionetas,
-            siempre con la garantía de calidad y durabilidad que nos
-            caracteriza. Además, nuestro equipo de profesionales te brindará un
-            servicio integral para que disfrutes al máximo de tu inversión.
+            En <span className="text-orange-500">ARS Autocamiones</span>, la
+            tradición de ENCAVA se une a la atención personalizada. Como
+            concesionario oficial, te ofrecemos la gama completa de vehículos
+            ENCAVA, desde autobuses hasta camionetas, siempre con la garantía de
+            calidad y durabilidad que nos caracteriza. Además, nuestro equipo de
+            profesionales te brindará un servicio integral para que disfrutes al
+            máximo de tu inversión.
           </p>
           <img
             className="w-full rounded"
@@ -38,27 +39,31 @@ function SobrePage() {
               <div>
                 <div className="max-w-lg md:max-w-none">
                   <p className="mt-4 text-normal font-bold xl:text-xl text-justify">
-                    ENCAVA, fundada en 1962, ha dejado una huella imborrable en
-                    el sector del transporte venezolano durante más de seis
-                    décadas. Su compromiso con la innovación y la satisfacción
-                    de las necesidades de movilidad del país la ha convertido en
-                    un referente en el mercado.<br></br> Desde sus inicios, los
-                    autobuses de ENCAVA han sido pioneros en la evolución del
-                    transporte público en Venezuela. La empresa se estableció
-                    con el objetivo de ofrecer una combinación óptima de
-                    calidad, diseño, innovación, servicio y comprensión de las
-                    necesidades del paí.<br></br>ose en sus primeros logros y
-                    con un enfoque constante en la excelencia en todas las áreas
-                    de su negocio,<br></br>mprendió un camino de crecimiento
-                    sostenido. Este camino no solo transformó a la empresa, sino
-                    también a todo el parque vehicular dedicado al transporte
-                    colectivo en el país, incluso antes de que ENCAVA cumpliera
-                    su primera década. En su evolución, ENCAVA se trasladó a un
-                    espacio diseñado para cumplir con sus objetivos: un terreno
-                    de 110 mil metros cuadrados.<br></br>acio ha permitido las
-                    inversiones necesarias en infraestructura, capital humano y
-                    tecnología para consolidar su posición de liderazgo en el
-                    sector. 
+                    <span className="text-orange-500">ENCAVA</span>, fundada en
+                    1962, ha dejado una huella imborrable en el sector del
+                    transporte venezolano durante más de seis décadas. Su
+                    compromiso con la innovación y la satisfacción de las
+                    necesidades de movilidad del país la ha convertido en un
+                    referente en el mercado.<br></br> Desde sus inicios, los
+                    autobuses de <span className="text-orange-500">ENCAVA</span>{" "}
+                    han sido pioneros en la evolución del transporte público en
+                    Venezuela. La empresa se estableció con el objetivo de
+                    ofrecer una combinación óptima de calidad, diseño,
+                    innovación, servicio y comprensión de las necesidades del
+                    paí.<br></br>ose en sus primeros logros y con un enfoque
+                    constante en la excelencia en todas las áreas de su negocio,
+                    <br></br>mprendió un camino de crecimiento sostenido. Este
+                    camino no solo transformó a la empresa, sino también a todo
+                    el parque vehicular dedicado al transporte colectivo en el
+                    país, incluso antes de que{" "}
+                    <span className="text-orange-500">ENCAVA</span> cumpliera su
+                    primera década. En su evolución,{" "}
+                    <span className="text-orange-500">ENCAVA</span> se trasladó
+                    a un espacio diseñado para cumplir con sus objetivos: un
+                    terreno de 110 mil metros cuadrados.<br></br>acio ha
+                    permitido las inversiones necesarias en infraestructura,
+                    capital humano y tecnología para consolidar su posición de
+                    liderazgo en el sector. 
                   </p>
                 </div>
               </div>
@@ -73,46 +78,81 @@ function SobrePage() {
             </div>
           </div>
         </section>
-        <div className="flex justify-center mt-96">
-          <button
-            onClick={() => cambiarParrafo(0)}
-            className="border hover:border-orange-700 border-black"
-          >
-            1
-          </button>
-          <button
-            onClick={() => cambiarParrafo(1)}
-            className="border  hover:border-orange-700 border-black"
-          >
-            2
-          </button>
-          <button
-            onClick={() => cambiarParrafo(2)}
-            className="border  hover:border-orange-700 border-black"
-          >
-            3
-          </button>
-        </div>
-        <span>
-          <p className={selectedParrafo === 0 ? "" : "hidden"}>
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-            Aspernatur, nisi vero quae ipsam repellendus exercitationem
-            consequatur eum animi consectetur, nihil, dolorum incidunt. Ex
-            officiis hic atque nostrum, reprehenderit quibusdam illum.
-          </p>
-          <p className={selectedParrafo === 1 ? "" : "hidden"}>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus
-            sint illo illum dicta quasi dolor. Provident fugiat numquam tempora
-            ipsam quaerat qui corrupti, sed non quis atque eaque asperiores
-            eius.
-          </p>
-          <p className={selectedParrafo === 2 ? "" : "hidden"}>
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-            Repudiandae iste minus accusantium neque similique rerum dolore
-            corporis magni, nobis voluptates ducimus ea eligendi odit laboriosam
-            pariatur ipsam animi! Sint, nobis?
-          </p>
-        </span>
+        <section className="mx-auto py-24">
+          <div className="container">
+            <div className="rounded-2xl shadow-xl grid lg:grid-cols-2 gap-8 items-center bg-white">
+              <div className=" flex items-center justify-center h-[400px] ">
+                <img
+                  src="https://s3-alpha-sig.figma.com/img/7177/e442/a4e5233d7e51357157ecb013e48a1c0a?Expires=1740960000&Key-Pair-Id=APKAQ4GOSFWCW27IBOMQ&Signature=nyJ4MhL2gcUTMS3N6Uv9h~A0Rj-1JwxYmWEyq7yfNY1M3GgbD7repD6Hqh9NDdfAiPCeN-8kz~Cww0Rym3kOChg0a8NzCk7xvfTvDhzyP64B-bUIfIRLHz5ASuQ4xkQTjAbcZLxqfLJM7EGhy~r2I8vFjGv7DNwR4TsL4rR4RNW9Pkobvnz2OwfA3chlEjlJ9gJAG3bDb25g29n21XD-yNCSnIaKnZGEq7Ff3ERoju~9R7ztWWF~1rL7eeyK~K0aRb4qktL9UmZlgua87QOAR7ONjWD1kVKxjCHb5adVhjPsEEGtvBxl~sCTt88HJhep1C9dgAYc73ChZcQ7w5vrxg__"
+                  width="350"
+                  alt="Placeholder image"
+                ></img>
+              </div>
+              <div className=" flex flex-col items-center lg:items-start justify-center">
+                <SubtituloTipo1 texto="MISION" />
+                <p className="text-center lg:text-left text-lg font-bold mb-8 max-w-xl">
+                  Fabricar vehículos confiables a la medida de las necesidades
+                  de nuestros clientes y usuarios.
+                </p>
+              </div>
+            </div>
+          </div>
+        </section>
+        <section className="mx-auto py-24">
+          <div className="container">
+            <div className="rounded-2xl shadow-xl grid lg:grid-cols-2 gap-8 items-center bg-white">
+              <div className=" flex items-center justify-center h-[400px] ">
+                <img
+                  src="https://s3-alpha-sig.figma.com/img/7220/d4d6/92b7c072022c123bd071b9ef2c49c004?Expires=1740960000&Key-Pair-Id=APKAQ4GOSFWCW27IBOMQ&Signature=jo2v2jJfdnneMT4A3ftkVpeqVRBbK8Y7fUxWHJWLp4VHVbgBjJFKScGjYaelIL6-M3uTLZxw9fHFsyjr~zFBxS3QaY2nnbd8VI-EauG9cp577c8NDajeEAL9gYAIuI4sFLbskM241heAoSByhHzOwn5GS~VU14pM225L0J1kD1Pcr-C6zlGzeEfH~MaUvOb64~kaXuoDAACObdmcu06knXie1YcNZ6HqX9j7neVEhYJdu1wNXflAFOPt1JfkfXHz3kRzrMT7gFoATzNidQcGmtUNIPdXHci4Ngla2wIIdavxmdixMPGB3A-3LXBBk5oa2Haeh14UBaBoKVta1exGJw__"
+                  width="350"
+                  alt="Placeholder image"
+                ></img>
+              </div>
+              <div className=" flex flex-col items-center lg:items-start justify-center">
+                <SubtituloTipo1 texto="VISION" />
+                <p className="text-center lg:text-left text-lg font-bold mb-8 max-w-xl">
+                  <span className="text-orange-500">ENCAVA</span>tiene por norte
+                  ser una empresa líder en el mercado. reconocida nacional e
+                  internacionalmente por su excelencia en la fabricación de
+                  autobuses y mini-buses gracias a la sistemática búsqueda de la
+                  calidad en todos sus productos y servicios, y al aporte
+                  fundamental de un personal altamente calificado y comprometido
+                  con la satisfacción de nuestros clientes.
+                </p>
+              </div>
+            </div>
+          </div>
+        </section>
+        <section className="mx-auto py-24">
+          <div className="container">
+            <div className="rounded-2xl shadow-xl grid lg:grid-cols-2 gap-8 items-center bg-white">
+              <div className=" flex items-center justify-center h-[400px] ">
+                <img
+                  src="https://s3-alpha-sig.figma.com/img/d7c9/c231/92eee3209255fb1e421681fbad6286be?Expires=1740960000&Key-Pair-Id=APKAQ4GOSFWCW27IBOMQ&Signature=J8Ccr9Mb4PRz5NM8NvYQr4IFYMqgQt6b9w4nB-GmpaFu85Mqkz-hNl1LAi77b7FiHIq-ZT3FN6GyFWkNAo-xzRVPF5EibtzFOCx7lYhj2g2H~o4N1xpoYUKu8qkYVfUjZLuOFx4FXjLBFTOb7sZ1AneN1SOYTmAH0OGUhIqrRO5gHFAIrXQuSMbnECRjeMv~GXcZUemUxcF1BNgOWfBYjgHPX6iiF94l4T1SXJ6Mfxav0KYzGXtQj1Z1NG6RvhGsL53NIEQxaYhDiUiq3mFizilYt~GAz9K45uBTqddKUv3ocdA6Tiq3zgA-DHYjcucJj1ANrNFTHfqF0peaHmhHdw__"
+                  width="350"
+                  alt="Placeholder image"
+                ></img>
+              </div>
+              <div className=" flex flex-col items-center lg:items-start justify-center">
+                <SubtituloTipo1 texto="FUTURO" />
+                <p className="text-center lg:text-left text-lg font-bold mb-8 max-w-xl">
+                  <span className="text-orange-500">ENCAVA</span>ha demostrado
+                  ser como sus autobuses: confiable. resistente, cumplidora. Por
+                  eso transita hacia el futuro con la confianza de haber
+                  demostrado suficientemente su compromiso con las cosas bien
+                  hechas. Seguir ascendiendo requerirá, entonces, dar
+                  continuidad a una gestión caracterizada por el conocimiento de
+                  las necesidades del cliente, la alta calidad en productos y
+                  servicios, la inversión en tecnología, la búsqueda permanente
+                  de la seguridad y el confort, la innovación permanente y el
+                  incremento sostenido del contenido nacional, por nombrar solo
+                  algunos de los ingredientes que han compuesto la fórmula del
+                  éxito.
+                </p>
+              </div>
+            </div>
+          </div>
+        </section>
       </div>
       <Footerbar />
     </>
